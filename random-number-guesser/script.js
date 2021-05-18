f11b533d399ab70f28c9de4741252688f73c0c28
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2021-05-18 15:52:33 +0530

    Create script.js

---
+++ random-number-guesser/script.js
@@ -0,0 +1,28 @@
+document.getElementById('btn').addEventListener('click', compare);
+
+        let guess = 5;
+        document.getElementById('guess').innerHTML = "Guesses Left: " + guess;
+
+        function compare(){
+
+            let comp = Math.floor(Math.random() * 10) + 1;
+            let human = document.getElementById('user').value;
+            
+            if(comp == human){
+                document.getElementById('result').innerHTML = 'Similar';
+            }else{
+                document.getElementById('guess').innerHTML = guess-1;
+                guess = guess -1;
+                document.getElementById('result').innerHTML = 'Different';
+                if(guess == 0){
+                    document.getElementById('app').remove();
+                    document.getElementById('relo').style.visibility = 'visible'; 
+
+                    document.getElementById('relo').innerHTML = `
+                    <h2> You ran out of guesses! </h2>  <br>
+                    <button onclick = 'location.reload()'>Click Here To Play Again</button>
+                    `;
+                }
+            }
+          
+        }
