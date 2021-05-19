a4bd07d999b7d8a27fc304fb2e4dc124a3772496
Sanchit <sanchit0496@gmail.com>
2021-05-19 10:46:10 +0530

    updating CSS

---
+++ introduce-yourself-angular/script.js
@@ -0,0 +1,13 @@
+var app=angular.module('myApp',[]);
+app.controller('myCtrl', function($scope){
+  $scope.firstName = "";
+  $scope.lastName = "";
+  $scope.compName = "";
+  $scope.desgName = "";
+  $scope.dojName = "";
+  $scope.stName = "";
+  $scope.ctName = "";
+  $scope.msgName = "";
+
+
+});
